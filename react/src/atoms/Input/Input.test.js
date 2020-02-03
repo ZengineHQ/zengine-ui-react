2bb9d4d5ecc534789f9f82603454d41aa82e22e0
Alex Weber <alexweber@mailfence.com>
2020-02-03 15:42:21 -0300

    test: update input component tests

--- react/src/atoms/Input/Input.test.js
+++ react/src/atoms/Input/Input.test.js
@@ -12,3 +12,28 @@ test('Marks input as required when specified', () => {
   const { container } = render(<Input required={ true }/>);
   expect(container.firstChild).toHaveAttribute('required');
 });
+
+test('Sets aria-required attribute when required', () => {
+  const { container } = render(<Input required={ true }/>);
+  expect(container.firstChild).toHaveAttribute('aria-required', 'true');
+});
+
+test('Marks input as disabled when specified', () => {
+  const { container } = render(<Input disabled={ true }/>);
+  expect(container.firstChild).toHaveAttribute('disabled');
+});
+
+test('Sets aria-disabled attribute when disabled', () => {
+  const { container } = render(<Input disabled={ true }/>);
+  expect(container.firstChild).toHaveAttribute('aria-disabled', 'true');
+});
+
+test('Sets input placeholder when specified', () => {
+  const { container } = render(<Input placeholder={ 'foo' }/>);
+  expect(container.firstChild).toHaveAttribute('placeholder', 'foo');
+});
+
+test('Sets input id when specified', () => {
+  const { container } = render(<Input id={ 'foo' }/>);
+  expect(container.firstChild).toHaveAttribute('id', 'foo');
+});
--- react/src/molecules/inputs/TextInput/TextInput.test.js
+++ react/src/molecules/inputs/TextInput/TextInput.test.js
@@ -3,17 +3,56 @@ import { render } from '@testing-library/react';
 
 import TextInput from './TextInput';
 
-test('Renders text input', () => {
-  const { container } = render(<TextInput label="foo" />);
+test('Renders a text input', () => {
+  const { container } = render(<TextInput />);
   expect(container.getElementsByTagName('input')[0]).toHaveAttribute('type', 'text');
 });
 
 test('Sets label when specified', () => {
-  const { getByText } = render(<TextInput label="foo" />);
-  expect(getByText('foo')).toBeTruthy()
+  const { container, getByText } = render(<TextInput label="foo" />);
+  expect(getByText('foo')).toBeTruthy();
+
+  const labels = container.getElementsByTagName('label');
+  expect(labels.length).toEqual(1);
 });
 
 test('Marks input as required when specified', () => {
-  const { container } = render(<TextInput required={ true } label="foo" />);
+  const { container } = render(<TextInput required={ true } />);
   expect(container.getElementsByTagName('input')[0]).toHaveAttribute('required');
+});
+
+test('Sets area-required attribute when required', () => {
+  const { container } = render(<TextInput required={ true } />);
+  expect(container.getElementsByTagName('input')[0]).toHaveAttribute('aria-required', 'true');
+});
+
+test('Marks input as disabled when specified', () => {
+  const { container } = render(<TextInput disabled={ true } />);
+  expect(container.getElementsByTagName('input')[0]).toHaveAttribute('disabled');
+});
+
+test('Sets area-disabled attribute when disabled', () => {
+  const { container } = render(<TextInput disabled={ true } />);
+  expect(container.getElementsByTagName('input')[0]).toHaveAttribute('aria-disabled', 'true');
+});
+
+test('Sets input placeholder when specified', () => {
+  const { container } = render(<TextInput placeholder={ 'foo' }/>);
+  expect(container.getElementsByTagName('input')[0]).toHaveAttribute('placeholder', 'foo');
+});
+
+test('Sets input id when specified', () => {
+  const { container } = render(<TextInput id={ 'foo' }/>);
+  expect(container.getElementsByTagName('input')[0]).toHaveAttribute('id', 'foo');
+});
+
+test('Sets label "for" attribute when id specified', () => {
+  const { container } = render(<TextInput label="Foo" id={ 'foo' }/>);
+  expect(container.getElementsByTagName('label')[0]).toHaveAttribute('for', 'foo');
+});
+
+test('Omits label element when not specified', () => {
+  const { container } = render(<TextInput />);
+  const labels = container.getElementsByTagName('label');
+  expect(labels.length).toEqual(0);
 });
