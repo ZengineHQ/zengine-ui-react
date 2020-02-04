9907f93bd4a1b71b2a8285c7c70851b2b68866aa
Alex Weber <alexweber@mailfence.com>
2020-02-04 18:03:11 -0300

    chore: create exportable version

--- react/src/api/index.js
+++ react/src/api/index.js
@@ -1,10 +1,10 @@
 // Atoms.
 
-export { Button } from './atoms';
-export { Input } from './atoms'
-export { Label } from './atoms'
+export { default as Button } from './atoms/Button/Button';
+export { default as Input } from './atoms/Input/Input';
+export { default as Label } from './atoms/Label/Label';
 
 // Molecules.
 
-export { NumberInput } from './molecules';
-export { TextInput } from './molecules';
+export { default as NumberInput } from './molecules/inputs/NumberInput/NumberInput';
+export { default as TextInput } from './molecules/inputs/TextInput/TextInput';
--- react/src/api/molecules/index.js
+++
@@ -1,2 +0,0 @@
-export { default as NumberInput } from './inputs/NumberInput/NumberInput';
-export { default as TextInput } from './inputs/TextInput/TextInput';
