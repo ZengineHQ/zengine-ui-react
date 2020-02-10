576dde690b1c7dfc6d1d825daab2afaefbfc64ff
Alex Weber <alexweber@mailfence.com>
2020-02-10 15:38:59 -0300

    chore: add console plugin

--- react/.storybook/preview.js
+++ react/.storybook/preview.js
@@ -1,9 +1,12 @@
 import { addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withTests } from '@storybook/addon-jest';
+import '@storybook/addon-console';
+import { withConsole } from '@storybook/addon-console';
 
 import results from '../src/.test-results.json';
 
 addDecorator(withKnobs);
 addDecorator(withTests({ results }));
 addParameters({ options: { showPanel: true }});
+addDecorator((storyFn, context) => withConsole()(storyFn)(context));
