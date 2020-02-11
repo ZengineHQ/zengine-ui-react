50bcb4b62900f41c6afd0549b4006197c18067be
Alex Weber <alexweber@mailfence.com>
2020-02-11 13:41:40 -0300

    chore: export missing components

--- react/src/api/index.js
+++ react/src/api/index.js
@@ -2,10 +2,13 @@
 export { default as Button } from './atoms/Button/Button';
 export { default as Input } from './atoms/Input/Input';
 export { default as Label } from './atoms/Label/Label';
+export { default as PageTitle } from './atoms/PageTitle/PageTitle';
+export { default as SectionHeader } from './atoms/SectionHeader/SectionHeader';
 
 // Molecules.
 export { default as NumberInput } from './molecules/inputs/NumberInput/NumberInput';
 export { default as TextInput } from './molecules/inputs/TextInput/TextInput';
+export { default as Select } from './molecules/inputs/Select/Select';
 
 // Organisms.
 export { default as SimpleSearch } from './organisms/forms/SimpleSearch/SimpleSearch';
