04566f25768b5b8aa458f31504d37836cccba18d
Alex Weber <alexweber@mailfence.com>
2020-02-13 09:52:25 -0300

    chore: no need for a table dashboard playground

--- react/src/api/molecules/TableDashboard/TableDashboard.stories.js
+++ react/src/api/molecules/TableDashboard/TableDashboard.stories.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { text } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 
 import { Button } from '../../index';
 import TableDashboard from './TableDashboard';
-import useDefaultPanel from '../../util/useDefaultPanel';
 
 export default {
   title: 'Organisms/TableDashboard',
@@ -39,36 +37,3 @@ export const ActionButton = () => {
     />
   );
 };
-
-// export const Playground = () => {
-//   useDefaultPanel('Knobs');
-//
-//   const headers = [
-//     text('Header Col 1', 'Title'),
-//     text('Header Col 2', 'Email'),
-//     text('Header Col 3', 'Age'),
-//   ];
-//
-//   const rows = [
-//     [
-//       text('Row 1 Col 1', 'John Smith'),
-//       text('Row 1 Col 2', 'john@smith.com'),
-//       text('Row 1 Col 3', '25'),
-//     ],
-//     [
-//       text('Row 2 Col 1', 'Jane Doe'),
-//       text('Row 2 Col 2', 'janedoe@hotmail.com'),
-//       text('Row 2 Col 3', '25'),
-//     ],
-//   ];
-//
-//   return (
-//     <TableDashboard headers={ headers } rows={ rows }>
-//       { ({ row, index }) => (
-//       <Button onClick={ action(`Button clicked for row ${ index }`) }>
-//         { text('Button Label', 'Edit') }
-//       </Button>
-//       ) }
-//     </TableDashboard>
-//   );
-// };
