4453e903652e027d2615ab098abf02f15f5975f9
Alex Weber <alexweber@mailfence.com>
2020-02-21 11:16:08 -0300

    chore: wip kitchen sink styleguide story

---
+++ react/src/storybook/templates/kitchen-sink.js
@@ -0,0 +1,45 @@
+// import React from 'react';
+//
+// import { PageTitle, SectionHeader } from '../../api';
+//
+// export default {
+//   title: 'Templates',
+//   parameters: {
+//     options: {
+//       showPanel: false
+//     }
+//   }
+// };
+//
+// export const KitchenSink = () => (
+//   <article>
+//     <PageTitle>
+//       Yidhra <small className="d-block">The Wizehive Atomic Style Guide</small>
+//     </PageTitle>
+//
+//     <div className="bg-primary text-white">
+//       <PageTitle>Atomic Components</PageTitle>
+//     </div>
+//
+//     <div className="bg-dark text-white">
+//       <SectionHeader>Atoms</SectionHeader>
+//     </div>
+//
+//
+//
+//     <div className="bg-dark text-white">
+//       <SectionHeader>Molecules</SectionHeader>
+//     </div>
+//
+//
+//     <div className="bg-dark text-white">
+//       <SectionHeader>Organisms</SectionHeader>
+//     </div>
+//
+//
+//     <div className="bg-dark text-white">
+//       <SectionHeader>Templates</SectionHeader>
+//     </div>
+//
+//   </article>
+// );
