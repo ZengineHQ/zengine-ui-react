ea5d0a220b4d72db204b9cd0189557bbc6f68c79
Alex Weber <alexweber@mailfence.com>
2020-02-25 08:01:39 -0300

    chore: update exports

--- react/src/api/index.js
+++ react/src/api/index.js
@@ -5,6 +5,7 @@ export { default as Image } from './atoms/Image/Image';
 export { default as Input } from './atoms/Input/Input';
 export { default as Label } from './atoms/Label/Label';
 export { default as PageTitle } from './atoms/PageTitle/PageTitle';
+export { default as Radio } from './atoms/Radio/Radio';
 export { default as SectionHeader } from './atoms/SectionHeader/SectionHeader';
 export { default as Select } from './atoms/Select/Select';
 export { default as Textarea } from './atoms/Textarea/Textarea';
@@ -13,6 +14,7 @@ export { default as Textarea } from './atoms/Textarea/Textarea';
 export { default as CheckboxField } from './molecules/CheckboxField/CheckboxField';
 export { default as CounterLabeled } from './molecules/CounterLabeled/CounterLabeled';
 export { default as NumberField } from './molecules/NumberField/NumberField';
+export { default as RadioGroupField } from './molecules/RadioGroupField/RadioGroupField';
 export { default as SelectField } from './molecules/SelectField/SelectField';
 export { default as TextAreaField } from './molecules/TextAreaField/TextAreaField';
 export { default as TextField } from './molecules/TextField/TextField';
@@ -23,6 +25,7 @@ export { default as MenuSlideOut } from './organisms/MenuSlideOut/MenuSlideOut';
 export { default as Table } from './organisms/Table/Table';
 export { default as Widget } from './organisms/Widget/Widget';
 
+// These are some example organisms nobody asked for, we can probably move to Templates for reference.
 export { default as ContactForm } from './organisms/ContactForm/ContactForm';
 export { default as QuickSearch } from './organisms/QuickSearch/QuickSearch';
 
