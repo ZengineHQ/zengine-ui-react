38b4ebc8f80028e438c1ce6c71859f31443681e6
Alex Weber <alexweber@mailfence.com>
2020-02-25 12:18:44 -0300

    chore: docs

--- react/src/api/util/withForwardRef.js
+++ react/src/api/util/withForwardRef.js
@@ -1,5 +1,11 @@
 import React, { forwardRef } from 'react';
 
+/**
+ * Wraps a component with an injected inner reference so that it can be attached inside of it.
+ *
+ * This is useful for components that wrap HTML elements such as inputs where you might want a reference to the
+ * actual input element itself from outside of the component.
+ */
 export default function withForwardRef (Component) {
   function component (props, ref) {
     return <Component innerRef={ ref } { ...props } />;
