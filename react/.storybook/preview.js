b501a7b7fef1323e4147e388c5b4d1597820a0aa
Alex Weber <alexweber@mailfence.com>
2020-02-26 09:06:39 -0300

    chore: dont fail to start storybook if no test results found

--- react/.storybook/preview.js
+++ react/.storybook/preview.js
@@ -2,10 +2,14 @@ import { addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withTests } from '@storybook/addon-jest';
 
-import results from '../src/test/.results.json';
+try {
+  const results = require('../src/test/.results.json');
+  addDecorator(withTests({ results }));
+} catch (e) {
+  // It's all good, just don't display test results.
+}
 
 addDecorator(withKnobs);
-addDecorator(withTests({ results }));
 addParameters({
   options: { showPanel: true },
   viewMode: 'story',
