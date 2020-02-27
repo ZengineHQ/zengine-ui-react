f7f0709ff723c80bc1e85c61e8bcf80a4757340c
Alex Weber <alexweber@mailfence.com>
2020-02-27 10:01:17 -0300

    chore: add some extra validation functions + tests

--- react/src/api/util/validation.js
+++ react/src/api/util/validation.js
@@ -24,3 +24,37 @@ export const isEmpty = value => {
   }
   return value === '' || !exists(value);
 };
+
+/**
+ * Validates whether a value is a string.
+ *
+ * @param {*} value
+ * @returns {boolean}
+ */
+export const isString = value => typeof value === 'string';
+
+/**
+ * Validates whether a value matches a regex.
+ *
+ * @param {string} str
+ * @param {RegExp} regex
+ * @returns {boolean}
+ */
+export const matchesRegex = (str, regex) => {
+  if (!isString(str)) {
+    return false;
+  }
+  const validationRegex = (regex instanceof RegExp ? regex : (new RegExp(regex)));
+  return (isEmpty(str) || validationRegex.test(str));
+};
+
+/**
+ * Validates whether a value is an email address.
+ *
+ * @param {string} string
+ * @returns {boolean}
+ */
+export const isEmail = string => {
+  // eslint-disable-next-line
+  return matchesRegex(string, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i);
+};
