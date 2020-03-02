933d487a9c2f4eebf48e06c9a211fa13b18ec62d
Alex Weber <alexweber@mailfence.com>
2020-03-02 13:51:15 -0300

    docs: fix menu slide out in portals template implementation

--- src/storybook/templates/program-manager-portals.js
+++ src/storybook/templates/program-manager-portals.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Table, Widget } from '../../api';
 import useSyntaxHighlighter from '../useSyntaxHighlighter';
 import SectionHeader from '../../api/atoms/SectionHeader/SectionHeader';
+import MenuSlideOut from '../../api/organisms/MenuSlideOut/MenuSlideOut';
 
 export default {
   title: 'Templates/ProgramManager',
@@ -18,27 +19,39 @@ export default {
   }
 };
 
+const DemoData = ({ row }) => (
+  <ul>
+    <li>Form Name 01</li>
+    <li>Form Name 02</li>
+    <li>{ row[1] }</li>
+    <li>{ row[2] }</li>
+  </ul>
+);
+
 export const Portals = () => {
+  // In order to attach the slideOut to the table we must grab a ref for the table and pass it along.
+  const ref = useRef(null);
+
+  // Data fetching callback we pass the MenuSlideOut, this is done like this so we lazy-load all data used.
+  // It should return a component which displays the loaded data.
+  const getData = row => <DemoData row={ row } />;
+
   const headers = ['Title', 'Something Else', 'Date', ''];
   const rows = [
-    [
-      'Title Information',
-      'Some Info',
-      'March 2020',
-    ],
-    [
-      'Other Title',
-      'Other Info',
-      'June 2020',
-    ],
-    [
-      'Third Title',
-      'Third Info',
-      'April 29 1992',
-    ],
+    ['Title Information', 'Some Info', 'March 2020'],
+    ['Other Title', 'Other Info', 'June 2020'],
+    ['Third Title', 'Third Info', 'April 29 1992'],
   ].map(row => {
-    const button = null;
-    row.push(button);
+    const slideOut = (
+      <MenuSlideOut
+        target={ ref }
+        title={ `Associated Data ${ row[0] }` }
+        data={ () => getData(row) }
+      >
+        <Button theme="link">View Data</Button>
+      </MenuSlideOut>
+    );
+    row.push(slideOut);
     return row;
   });
 
@@ -58,7 +71,7 @@ export const Portals = () => {
       <SectionHeader>Process: Scholarship Award</SectionHeader>
 
       <Widget
-        body={ <Table headers={ headers } rows={ rows } classes="table-borderless table-hover"/> }
+        body={ <Table headers={ headers } rows={ rows } classes="table-borderless table-hover" /> }
         footer={ (
           <div className="text-center">
             <Button onClick={ action('clicked footer button') }>Set Assignments</Button>
@@ -66,7 +79,7 @@ export const Portals = () => {
         ) }
       />
 
-      <hr className="mt-5 mb-3"/>
+      <hr className="mt-5 mb-3" />
 
       This template utilizes the following components:
 
